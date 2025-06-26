4a479f39b6d7e1b288ada2dde674615a4a24cd94
Mohammed DS <92916932+mohamedds-12@users.noreply.github.com>
2025-06-26 17:26:35 +0100

    Fix RecordDelieveryJob not finding the sent_email model
    Used the has instead of the message_id cuz the message_id received is not the same as the message_id store in db

--- src/RecordDeliveryJob.php
+++ src/RecordDeliveryJob.php
@@ -32,7 +32,9 @@ class RecordDeliveryJob implements ShouldQueue
 
     public function handle()
     {
-        $sent_email = SentEmail::where('message_id', $this->message->mail->messageId)->first();
+        $emailHash = collect($this->message->mail->headers)->where('name', 'X-Mailer-Hash')->first()?->value;
+        $sent_email = SentEmail::where('message_id', $emailHash)->first();
+        
         if ($sent_email) {
             $meta = collect($sent_email->meta);
             $meta->put('smtpResponse', $this->message->delivery->smtpResponse);
